af31c69cf970b43cb5658d377bfb7554bd09a164
DANIEL J EGELHOFF <degelhoff@wisc.edu>
2025-03-08 15:52:03 -0600

    tests working

--- hw6/intro_pytorch.py
+++ hw6/intro_pytorch.py
@@ -4,17 +4,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
-
-
 def get_data_loader(training = True):
     """
-    TODO: implement this function.
-
-    INPUT: 
-        An optional boolean argument (default value is True for training dataset)
-
-    RETURNS:
-        Dataloader for the training set (if training = True) or the test set (if training = False)
+    INPUT: An optional boolean argument (default value is True for training dataset)
+    RETURNS: Dataloader for the training set (if training = True) or the test set (if training = False)
     """
     custom_transform=transforms.Compose([
         transforms.ToTensor(),
@@ -23,90 +16,138 @@ def get_data_loader(training = True):
 
     # load dataset with torchvision
     if training:
-        dataset = datasets.FashionMNIST('./data', True, download=True, transform=custom_transform)
+        dataset = datasets.FashionMNIST('./data', train=True, download=True, transform=custom_transform)
     else:
-        dataset = datasets.FashionMNIST('./data', False, True, custom_transform)
+        dataset = datasets.FashionMNIST('./data', train=False, download=True, transform=custom_transform)
     # retrieve images and labels from dataset object, shuffle if training set
-    loader = torch.utils.data.DataLoader(dataset, batch_size = 64, shuffle=training)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=training)
     return loader
-
 
 def build_model():
     """
-    TODO: implement this function
-
-    INPUT: 
-        None
-
-    RETURNS:
-        An untrained neural network model
+    INPUT: None
+    RETURNS: An untrained neural network model
     """
-
-
-
+    # model creation statement
+    model = nn.Sequential(
+        nn.Flatten(), 
+        nn.Linear(in_features=784, out_features=128), # dense layer
+        nn.ReLU(), 
+        nn.Linear(in_features=128, out_features=64), # dense layer
+        nn.ReLU(), 
+        nn.Linear(in_features=64, out_features=10) # dense layer (output)
+    )
+    return model
 
 def build_deeper_model():
     """
-    TODO: implement this function.
-
-    INPUT: 
-        None
-
-    RETURNS:
-        An untrained neural network model
+    INPUT: None
+    RETURNS: An untrained neural network model
     """
-
+    model = nn.Sequential(
+        nn.Flatten(), 
+        nn.Linear(in_features=784, out_features=256), # dense layer
+        nn.ReLU(), 
+        nn.Linear(in_features=256, out_features=128), # dense layer
+        nn.ReLU(), 
+        nn.Linear(in_features=128, out_features=64), # dense layer
+        nn.ReLU(), 
+        nn.Linear(in_features=64, out_features=32), # dense layer
+        nn.ReLU(), 
+        nn.Linear(in_features=32, out_features=10) # dense layer OUTPUT
+    )
+    return model
 
 
 def train_model(model, train_loader, criterion, T):
     """
-    TODO: implement this function.
-
     INPUT: 
         model - the model produced by the previous function
         train_loader  - the train DataLoader produced by the first function
         criterion   - cross-entropy 
         T - number of epochs for training
-
-    RETURNS:
-        None
+    RETURNS: None
     """
-    
+    # optimazation algortihm: stochastic gradient descent (SGD)
+    opt = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    model.train() # set "mode" of model to training
+    for epoch in range(T):
+        running_loss = 0.0 # track loss for this epoch
+        running_correct = 0.0 # calculate accuracy for this epoch (correct / 60000)
+        num_batches = 0; # used to average loss across all batches each epoch
+        for data in train_loader:
+            # get inputs
+            inputs, labels = data
+            # zero gradients
+            opt.zero_grad();
+            # feed inputs to model to generate outputs
+            outputs = model(inputs)
+            # calculate loss
+            loss = criterion()(outputs, labels)
+            loss.backward()
+            opt.step()
+
+            # get predicted class -> determines if model predicted correct label
+            _, predicted = torch.max(outputs, dim=1)
+
+            # update loss, total, correct
+            running_loss += loss.item()
+            running_correct += (predicted == labels).sum().item()
+
+            # update batch counter
+            num_batches += 1
+
+        # print stats for each epoch
+        accuracy = 100*(running_correct/60000)
+        e_loss = running_loss/num_batches # not all batches equal size
+        print('Train Epoch:', epoch, '\t', end='  ')
+        print('Accuracy: ', int(running_correct),'/',60000,'(',f"{accuracy:.2f}", '%)', sep='', end='  ')
+        print('Loss:', f"{e_loss:.3f}")
 
 
 def evaluate_model(model, test_loader, criterion, show_loss = True):
     """
-    TODO: implement this function.
-
     INPUT: 
         model - the the trained model produced by the previous function
         test_loader    - the test DataLoader
-        criterion   - cropy-entropy 
-
-    RETURNS:
-        None
+        criterion   - cross-entropy 
+    RETURNS: None
     """
-    
+    model.eval()
+    correct = 0 # to calculate accuracy of test
+    total_loss = 0 # accumulate losses of each batch
+    num_samples = 0
+    with torch.no_grad():
+        for data in test_loader:
+            inputs, labels = data
+            outputs = model(inputs)
+            # number of correct labelings achieved by model
+            _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
+            # calculate loss: weighted by batch size
+            loss = criterion()(outputs, labels)
+            total_loss += loss.item() * labels.size(0)
+            num_samples += labels.size(0)
 
+    # print stats
+    if show_loss:
+        avg_loss = total_loss / num_samples
+        print('Average loss:', f"{avg_loss:.4f}")
+    accuracy = 100*(correct/num_samples)
+    print('Accuracy: ', f"{accuracy:.2f}", '%', sep='')
 
 def predict_label(model, test_images, index):
     """
-    TODO: implement this function.
-
     INPUT: 
         model - the trained model
         test_images   -  a tensor. test image set of shape Nx1x28x28
         index   -  specific index  i of the image to be tested: 0 <= i <= N - 1
-
-
-    RETURNS:
-        None
+    RETURNS: None
     """
-
-
-if __name__ == '__main__':
-    '''
-    Feel free to write your own test code here to exaime the correctness of your functions. 
-    Note that this part will not be graded.
-    '''
-    criterion = nn.CrossEntropyLoss()
+    class_names = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle Boot']
+    logits = model(test_images)
+    prob = F.softmax(logits, dim=1) # prob[index] = list of probablities
+    top_3_probs = sorted(zip(prob[index], class_names), reverse=True)[:3]
+    for label in top_3_probs:
+        p = 100*label[0].item()
+        print(label[1], ': ', f"{p:.2f}", '%', sep='')
--- hw6/tests.py
+++ hw6/tests.py
@@ -3,8 +3,23 @@ from intro_pytorch import *
 
 def main():
 
-    loader = get_data_loader(training=True)
-    print(type(loader))
-    print(loader.dataset)
+    # get data loaders for FashionMNSIT dataset
+    train_loader = get_data_loader(training=True)
+    test_loader = get_data_loader(training=False)
+
+    # build, train, and test regular model
+    model = build_model();
+    train_model(model, train_loader, nn.CrossEntropyLoss, 5)
+    evaluate_model(model, test_loader, criterion=nn.CrossEntropyLoss, show_loss=True)
+
+    # build, train and test deeper model
+    # deep_model = build_deeper_model();
+    # train_model(deep_model, train_loader, nn.CrossEntropyLoss, 10)
+    # evaluate_model(deep_model, test_loader, criterion=nn.CrossEntropyLoss, show_loss=True)
+
+    # predict a label for a specific test image
+    test_images = next(iter(test_loader))[0]
+    predict_label(model=model, test_images=test_images, index=1)
+
 
 main()
